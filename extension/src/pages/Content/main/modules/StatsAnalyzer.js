711a5c126d66ee6086fabb7f5a496a85cc775710
Rafal Figlus <rafal_figlus@wp.pl>
2022-09-06 11:36:47 +0200

    integration with helper class - StatisticsMenu

--- extension/src/pages/Content/main/modules/StatsAnalyzer.js
+++ extension/src/pages/Content/main/modules/StatsAnalyzer.js
@@ -4,16 +4,14 @@ import { get_local_datetime } from "../../../../utils/time_utils"
 import { DATABASE_KEYS } from "../../../config"
 import { get_statistics_element } from "../../utils/get_statistics_element"
 import { send_playback_data } from "../../../../utils/http_requests/send_playback_data"
+import { StatisticsMenu } from "../../utils/StatisticsMenu"
 
 
 
 export class StatsAnalyzer{
     constructor(){
-        this.element = undefined
         this.interval = undefined
-        this.videoIndex = undefined
-
-        this.record_count = 0
+        this.stats_menu = undefined
     }
 
     print(text){
@@ -23,100 +21,39 @@ export class StatsAnalyzer{
     async init(){
         this.print(`Initializing...`)
 
-        // Assigning video index
-        this.videoIndex = (await chrome.storage.local.get([STORAGE_KEYS.VIDEO_COUNT]))[STORAGE_KEYS.VIDEO_COUNT]
+        // Create StatisticsMenu class instance
+        this.stats_menu = new StatisticsMenu()
+        await this.stats_menu.init()
 
-        // Get statistics element to be analyzed
-        this.element = await get_statistics_element()
+        // Start recording playback statistics
+        await this.start_recording()
+    }
 
-
-
+    async start_recording(){
         // Start interval that will be killed in case of switching video to another
-        this.interval = setInterval(() => {
-            chrome.storage.local.get([STORAGE_KEYS.VIDEO_COUNT]).then(async res => {
-                const video_count = res[STORAGE_KEYS.VIDEO_COUNT]
-                const video_index = video_count - 1
-                
-                // Analyze data
-                const timestamp = get_local_datetime(new Date())
-                const data = await this.analyze_data(this.element.value.toString(), timestamp)
-                const archive = this.compile_archive(this.element.value.toString(), timestamp)
+        this.interval = setInterval(async () => {
+            
+            const timestamp = get_local_datetime(new Date())
 
-                // Send playback data to backend
-                // Not using await --> not waiting for response
-                /*await*/send_playback_data(data, archive)
-                
+            const data = this.stats_menu.analyze_statistics_text()
+            data[DATABASE_KEYS.TIMESTAMP] = timestamp
 
-                // Check if credits are available and remove container
-                await this.are_credits_available()
-            })
+            const archive = this.compile_archive(this.stats_menu.get_statistics_text(), timestamp)
+
+            // Send playback data to backend
+            // Not using await --> not waiting for response
+            /*await*/send_playback_data(data, archive)
+            
+            // Check if credits are available and remove container
+            await this.are_credits_available()
         }, STATS_RECORD_INTERVAL_MS)
     }
 
-    /**
-     * Utility method creates object with all data to be extracted from nerd stats (long) string
-     * @param {string} data 
-     * @param {string} timestamp 
-     * @returns {object} result
-     */
-    async analyze_data(data, timestamp){
-        const result = {
-            [DATABASE_KEYS.POSITION]: this.get_value("(Position:) ([0-9]+.[0-9]+)", 2, data),
-            [DATABASE_KEYS.VOLUME]: this.get_value("(Volume:) ([0-9]+)%", 2, data),
-            [DATABASE_KEYS.SEGMENT_POSITION]: this.get_value("(Segment Position:) ([0-9]+.[0-9]+)", 2, data),
-
-            [DATABASE_KEYS.PLAYER_STATE]: this.get_value("(Player state: )([a-zA-Z]+)", 2, data),
-            [DATABASE_KEYS.BUFFERING_STATE]: this.get_value("(Buffering state:) (.+)", 2, data),
-            [DATABASE_KEYS.RENDERING_STATE]: this.get_value("(Rendering state:) (.+)", 2, data),
-
-            [DATABASE_KEYS.PLAYING_BITRATE_AUDIO]: this.get_value("Playing bitrate \\(a\\/v\\):\\s*([0-9]+)\\s*\\/\\s*([0-9]+)", 1, data),
-            [DATABASE_KEYS.PLAYING_BITRATE_VIDEO]: this.get_value("Playing bitrate \\(a\\/v\\):\\s*([0-9]+)\\s*\\/\\s*([0-9]+)", 2, data),
-            [DATABASE_KEYS.RESOLUTION]: this.get_value("([0-9]+x[0-9]+)", 1, data),
-
-            [DATABASE_KEYS.PLAYING_VMAF]: this.get_value("Playing\/Buffering vmaf: ([0-9]+)\s*\/\s*([0-9]+)", 1, data),
-            [DATABASE_KEYS.BUFFERING_VMAF]: this.get_value("Playing\/Buffering vmaf: ([0-9]+)\s*\/\s*([0-9]+)", 2, data),
-
-            [DATABASE_KEYS.BUFFERING_BITRATE_AUDIO]: this.get_value("Buffering bitrate \\(a\\/v\\):\\s*([0-9]+)\\s*\\/\\s*([0-9]+)", 1, data),
-            [DATABASE_KEYS.BUFFERING_BITRATE_VIDEO]: this.get_value("Buffering bitrate \\(a\\/v\\):\\s*([0-9]+)\\s*\\/\\s*([0-9]+)", 2, data),
-
-            [DATABASE_KEYS.TOTAL_FRAMES]: this.get_value("Total Frames:\\s*([0-9]+)", 1, data),
-            [DATABASE_KEYS.TOTAL_DROPPED_FRAMES]: this.get_value("Total Dropped Frames:\\s*([0-9]+)", 1, data),
-            [DATABASE_KEYS.TOTAL_CORRUPTED_FRAMES]: this.get_value("Total Corrupted Frames:\\s*([0-9]+)", 1, data),
-
-            [DATABASE_KEYS.FRAMERATE]: this.get_value("Framerate: ([0-9]+.[0-9]+)", 1, data),
-            [DATABASE_KEYS.TIMESTAMP]: timestamp,
-            [DATABASE_KEYS.DURATION]: this.get_value("(Duration:) ([0-9]+.[0-9]+)", 2, data)
-        }
-        return result
-    }
 
     compile_archive = (data, timestamp) => {
         return {
             [ARCHIVE_KEYS.DATA]: data,
             [ARCHIVE_KEYS.TIMESTAMP]: timestamp
-        }
-    }
-
-    /**
-     * Utility method --> extracts useful data from nerds stats (long) string
-     * @param {string} regex 
-     * @param {number} group 
-     * @param {string} data 
-     * @returns {object|null}
-     */
-    get_value = (regex, group, data) => {
-        try{
-            let value = data.match(regex) ?? null
-            if(value != null){
-                return value[group]
-            }
-            else{
-                return null
-            }
-        }
-        catch(e){
-            console.log(e)
-            return null
         }
     }
 
