1aea7c232629a51090f0f8eef1dd3a8e1f429bad
Rafal Figlus <rafal_figlus@wp.pl>
2022-09-07 10:49:17 +0200

    bitrate menu invoke before bitrate change - fix

--- extension/src/pages/Content/main/modules/BitrateManager.js
+++ extension/src/pages/Content/main/modules/BitrateManager.js
@@ -2,7 +2,6 @@ import { CONFIGURATION_KEYS, STORAGE_KEYS } from "../../../config";
 import { BITRATE_CHANGE_INTERVAL } from "../../../config";
 import { get_local_datetime } from "../../../../utils/time_utils";
 import { send_bitrate } from "../../../../utils/http_requests/send_bitrate";
-import { invoke_bitrate_menu_and_get_html_elements } from "../../utils/get_bitrate_menu_elements";
 import { BitrateMenu } from "../../utils/BitrateMenu";
 import { CustomLogger } from "../../../../utils/CustomLogger";
 
@@ -11,11 +10,17 @@ export class BitrateManager{
         // BitrateMenu class instance
         this.bitrate_menu = undefined
 
+        // Episode's scenario
         this.scenario = undefined
 
+        // Scenario iterator
+        this.iterator = undefined
+
+        // Custom logger
         this.logger = new CustomLogger("[BitrateManager]")
     }
 
+    
     async init(){
         // Create bitrate menu class instance
         this.bitrate_menu = new BitrateMenu()
@@ -24,9 +29,16 @@ export class BitrateManager{
         // Prepare video's bitrate scenario
         await this.prepare_video_scenario()
 
+        // Create iterator
+        this.iterator = this.scenario_iterator()
+
+        // Set initial bitrate value
+        await this.set_initial_bitrate()
+
         // Start bitrate changes
-        await this.start_bitrate_changes()
+        await this.start_bitrate_changes_interval()
     }
+
 
     /**
      *  Method prepares scenario for current video.
@@ -41,6 +53,7 @@ export class BitrateManager{
         const scenario = configuration[CONFIGURATION_KEYS.VIDEOS][video_index][CONFIGURATION_KEYS.VIDEO_KEYS.SCENARIO]
         this.scenario = scenario
     }
+
 
     /**
      * Generator or iterator idk
@@ -63,33 +76,55 @@ export class BitrateManager{
 
 
     /**
+     *  Sets the initial bitrate value
+     *  which is the first in video's escenario 
+    */
+    async set_initial_bitrate(){
+        const initial_settings = this.iterator.next().value
+        this.logger.log("Setting initial bitrate value: " + initial_settings.bitrate)
+
+        await this.invoke_bitrate_change(initial_settings.bitrate)
+    }
+
+
+    /**
      * Starts bitrate changes in intervals
      * Bitrates are set according to the configuration's scenario for current video.
      * In case the provided bitrate is not available the closest available value will be applied.
      * Validation is part of BitrateMenu instance's methods
     */
-    start_bitrate_changes(){
-        // Start regular bitrate changes
-        const iterator = this.scenario_iterator()
-
+    start_bitrate_changes_interval(){
         setInterval(async () => {  
-            const current_settings = iterator.next().value    
+            const current_settings = this.iterator.next().value    
             this.logger.log(`Setting bitrate to ${current_settings.bitrate}kbps which corresponds to VMAF ${current_settings.vmaf}`)
             this.logger.log(`VMAF template was ${current_settings.vmaf_template}. Difference: ${current_settings.vmaf_diff}`)
                 
-            const bitrate_validated = await this.bitrate_menu.set_bitrate(current_settings.bitrate)
-            await this.send_bitrate_change_update(bitrate_validated)
+            await this.invoke_bitrate_change(current_settings.bitrate)
         }, BITRATE_CHANGE_INTERVAL)
     }
 
 
+    /**
+     * Invokes bitrate change first by invoking the bitrate menu, then
+     * validating provided bitrate and overriding playback bitrate with it.
+     * Also sends bitrate change post request to the backend server.
+     * @param {Number} bitrate 
+    */
+    async invoke_bitrate_change(bitrate){
+        // Invoke bitrate menu - watch the method's body in BitrateMenu class
+        await this.bitrate_menu.invoke_bitrate_menu()   // ESSENTIAL --> bitrate menu has to be invoked before simulating clicks and changing bitrate
+        // Validate selected bitrate
+        const bitrate_validated = await this.bitrate_menu.set_bitrate(bitrate)
+        // Send bitrate change update to backend server
+        await this.send_bitrate_change_update(bitrate_validated)
+    }
     
 
     /**
      * Prepares data and sends post request to REST API
      * with information on new bitrate change.
      * Also updates chrome.storage with current bitrate
-     * @param {number} bitrate 
+     * @param {Number} bitrate 
     */
     async send_bitrate_change_update(bitrate){
         // Get previous bitrate and send update
