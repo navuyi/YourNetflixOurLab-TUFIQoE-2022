869017a1e566d3dca40292354518998341530f91
navuyi <figlusrafal@gmail.com>
2022-09-27 15:13:37 +0200

    fix mac_start.sh

--- backend/mac_start.sh
+++ backend/mac_start.sh
@@ -1,11 +1,11 @@
-@ECHO OFF
 ECHO Starting Flask Application
 
 source "./venv/bin/activate"
+
+
 export FLASK_APP=app
 export FLASK_ENV=production    # <-- production mode (???)
 export FLASK_DEBUG=True        # <-- debug is ESSENTIAL in order for logging to work
 
 
-
-python -m flask run
+flask run
